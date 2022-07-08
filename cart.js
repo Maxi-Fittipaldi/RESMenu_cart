bbeea249d7b32b67e9df13f482296f43f29bdf61
Maxif Fitti <a.maximiliano.fittipaldi@schonthal.esc.edu.ar>
2022-07-08 10:01:40 -0300

    semi-funcional, falta implementar xml-httprequest

--- cart.js
+++ cart.js
@@ -2,10 +2,12 @@ const cart = document.getElementsByClassName("products-in-cart")[0];
 const productsContainer = document.getElementsByClassName("products-container")[0];
 const confirmButton = document.querySelector("body > div.cart > button");
 const addToCartButton = document.querySelectorAll(".addToCart");
-console.log(addToCartButton)
+let ids = [];
+console.log(addToCartButton);
 for(let i = 0; i < addToCartButton.length; i++){
     addToCartButton[i].addEventListener("click",() =>{
         let productId = addToCartButton[i].name;
+        ids.push(productId);
         console.log(productId);
         const product = productsContainer.querySelector("[name='"+productId+"']");
         const clonedProd = product.cloneNode(true);
@@ -17,14 +19,13 @@ for(let i = 0; i < addToCartButton.length; i++){
 }
 
 confirmButton.addEventListener("click", () => {
-    let ids = [];
-    const productsInCart = cart.querySelectorAll("div.product");
-    console.log(productsInCart[0].name)
     let data = new FormData();
     data.append("product", ids);
-
+    console.log(ids);
     let xml = new XMLHttpRequest();
-    xml.open("POST","/menu/commit")
-    xml.httpRequestHeader("Content-type","application/x-www-form-urlencoded")
-    xml.send(data);
+    xml.open("POST","/menu/commit",true)
+    xml.setRequestHeader("content-type","application/x-www-form-urlencoded;charset=UTF-8")
+    xml.send("product_ids=" + ids);
 });
+//ver esto
+// https://code-maven.com/slides/python/flask-and-ajax-plain-javascript
